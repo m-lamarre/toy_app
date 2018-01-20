8d82b66fc735712665a508fc8f2a68bb132c4c5d
m-lamarre <studium.square.pantz@gmail.com>
2018-01-21 01:40:23 +0200

    brand new app

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,26 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'slim'
-require 'sass'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ToyApp
+module TestApp
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+    config.sass.preferred_syntax = :sass
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+
+  if Rails.configuration.respond_to?(:sass)
+    Rails.configuration.sass.tap do |config|
+      config.preferred_syntax = :sass
+    end
+  end
+
 end
